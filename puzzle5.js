87ee4055ae55d432aa6a2954f54f21b02f2e47cf
srimmyrimrim <srim@cca.edu>
2015-09-06 10:04:36 -0700

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,43 @@
+right();
+right();
+getColor();
+if (getColor() == "blue" )
+	{
+    			down();
+    			right();
+    			right();
+  }
+			else
+ 	{
+   				up();
+   				right();
+   				right();
+  }
+	
+getColor();
+if (getColor() == "blue" )
+	{
+    			down();
+    			right();
+    			right();
+  }
+			else
+ 	{
+   				up();
+   				right();
+   				right();
+  }
+getColor();
+if (getColor() == "blue" )
+	{
+    			down();
+    			right();
+    			right();
+  }
+			else
+ 	{
+   				up();
+   				right();
+   				right();
+  }
+right();	
