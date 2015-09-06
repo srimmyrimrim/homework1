a4f697d0cae5e1ec98612156e6cc1fcb55636aa1
srimmyrimrim <srim@cca.edu>
2015-09-06 10:05:00 -0700

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,22 @@
+right();
+var color;
+getColor();
+color = getColor();
+if (getColor() == "blue" )
+	{
+    			down();
+    			down();
+    			down();
+    			setColor(color);
+				right();
+				down();
+  }
+			else
+ 	{
+   				up();
+    			up();
+   				up();
+   				setColor(color);
+					left();
+					up();
+  }
